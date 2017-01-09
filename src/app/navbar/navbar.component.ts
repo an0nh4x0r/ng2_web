ec4630cbc7cf2763cf57e195699b7a1f3e5ba46b
Swapnil Singh <swappp7singh@gmail.com>
2017-01-09 23:08:00 +0530

    modified navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  branding:string = "Ng2 Website";
+
   constructor() { }
 
   ngOnInit() {
